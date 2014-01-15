8c784a63013c2ea2a06a08a8cdbd791607e7a65e
Holger Liesegang <hl@liesegang-media.de>
2014-01-15 11:45:57 +0100

    Added some Icons…

--- Calc/Calc/LGAppDelegate.m
+++ Calc/Calc/LGAppDelegate.m
@@ -43,4 +43,8 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)prefersStatusBarHidden {
+    return YES;
+}
+
 @end
